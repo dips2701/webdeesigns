2516d931cdf28d1cfdd5f45cb40ae2d5611eeae0
dips <dipuwanjari@gmail.com>
2024-07-21 09:10:27 +0530

    Your commit message here

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      lineHeight: {
+        '400px': '400px', // Adding custom line height of 400px
+      }
+    },
+  },
+  plugins: [],
+}
+
